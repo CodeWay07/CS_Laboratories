567ee60b2a41feae81c3f00ed6852f0815d558ba
CodeWay07 <maxim.bugaescu.mb@gmail.com>
2022-11-02 22:08:23 +0200

    Lab_3 commit

---
+++ Lab_3/caesar_substitution.py
@@ -0,0 +1,26 @@
+from Ciphers.cipher import Cipher
+
+shift = 2
+alphabet = "abcdefghijklmnopqrstuvwxyz "
+letter_to_index = dict(zip(alphabet, range(len(alphabet))))
+index_to_letter = dict(zip(range(len(alphabet)), alphabet))
+
+def encrypt(message, shift, letter_to_index, index_to_letter):
+    cipher = ""
+
+    for letter in message:
+        number = (letter_to_index[letter] + shift) % len(letter_to_index)
+        letter = index_to_letter[number]
+        cipher += letter
+
+    return cipher
+
+def decrypt(cipher, shift, letter_to_index, index_to_letter):
+    decrypted = ""
+
+    for letter in cipher:
+        number = (letter_to_index[letter] - shift) % len(letter_to_index)
+        letter = index_to_letter[number]
+        decrypted += letter
+
+    return decrypted
---
+++ Lab_3/vigenere.py
@@ -0,0 +1,36 @@
+from Ciphers.cipher import Cipher
+
+alphabet = "abcdefghijklmnopqrstuvwxyz "
+letter_to_index = dict(zip(alphabet, range(len(alphabet))))
+index_to_letter = dict(zip(range(len(alphabet)), alphabet))
+
+
+def encrypt(message, key, alphabet, letter_to_index, index_to_letter):
+    encrypted = ""
+    split_message = [
+        message[i : i + len(key)] for i in range(0, len(message), len(key))
+    ]
+    for each_split in split_message:
+        i = 0
+        for letter in each_split:
+            number = (letter_to_index[letter] + letter_to_index[key[i]]) % len(alphabet)
+            encrypted += index_to_letter[number]
+            i += 1
+
+    return encrypted
+
+
+def decrypt(cipher, key, alphabet, letter_to_index, index_to_letter):
+    decrypted = ""
+    split_encrypted = [
+        cipher[i : i + len(key)] for i in range(0, len(cipher), len(key))
+    ]
+
+    for each_split in split_encrypted:
+        i = 0
+        for letter in each_split:
+            number = (letter_to_index[letter] - letter_to_index[key[i]]) % len(alphabet)
+            decrypted += index_to_letter[number]
+            i += 1
+
+    return decrypted
